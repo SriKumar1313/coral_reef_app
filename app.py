2f858412e2bdf289a4a39563db3cbf993977e595
SriKumar1313 <149764826+SriKumar1313@users.noreply.github.com>
2024-06-24 19:59:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,56 @@
 import streamlit as st
-import os
 import numpy as np
 import pickle
+import os
 from PIL import Image, ImageOps
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 # Function to preprocess the uploaded image
 def preprocess_image(image, img_size=(64, 64)):
-    img = ImageOps.fit(image, img_size, Image.ANTIALIAS)
-    img = ImageOps.grayscale(img)
+    img = ImageOps.fit(image, img_size, Image.LANCZOS)  # Resize using LANCZOS method
+    img = ImageOps.grayscale(img)  # Convert to grayscale
     img_array = np.array(img).flatten()
     return img_array
 
+# Function to load the SVM model and preprocessing components
+def load_model(model_path):
+    with open(model_path, 'rb') as file:
+        svm_model, scaler, pca = pickle.load(file)
+    return svm_model, scaler, pca
+
 # Function to make predictions
 def predict(image, model, scaler, pca):
-    # Preprocess image
     img_array = preprocess_image(image)
-    # Scale and apply PCA using the provided scaler and pca
     img_scaled = scaler.transform(img_array.reshape(1, -1))
     img_pca = pca.transform(img_scaled)
-    # Predict using the model
     prediction = model.predict(img_pca)
     return prediction
 
+# Main function to run the Streamlit web app
 def main():
-    st.title('Coral Image Classifier')
-    st.text('Upload a coral image to classify it as healthy or bleached')
-
+    st.title("Coral Reef Image Classifier")
+    st.sidebar.title("Upload Image")
+    
     # Upload image
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
+    uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.sidebar.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.sidebar.write("")
+        st.sidebar.write("Classifying...")
 
-        # Check if the model file exists
+        # Load SVM model and preprocessing components
         model_path = 'svm_model_pca.pkl'
         if not os.path.exists(model_path):
             st.write("Error: SVM model file not found.")
             return
-
-        # Load SVM model and preprocessing components
-        with open(model_path, 'rb') as file:
-            svm_model, scaler, pca = pickle.load(file)
+        
+        svm_model, scaler, pca = load_model(model_path)
 
         # Make prediction
         prediction = predict(image, svm_model, scaler, pca)
-
-        # Display prediction
-        categories = ['healthy', 'bleached']
+        categories = ['healthy_corals', 'bleached_corals']
         st.write(f"Prediction: {categories[prediction[0]]}")
 
 if __name__ == '__main__':
